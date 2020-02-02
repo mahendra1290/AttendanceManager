301bd1bd5db2fa19e4a5522ab23ce1c2cb338b9f
mahendra1290 <mahendra.suthar.1290@gmail.com>
2020-02-02 13:43:48 +0530

    single fragment changed from kotlin to java

--- app/src/main/java/android/mahendra/attendancemanager/SubjectListFragment.java
+++ app/src/main/java/android/mahendra/attendancemanager/SubjectListFragment.java
@@ -2,13 +2,11 @@ package android.mahendra.attendancemanager;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.mahendra.attendancemanager.databinding.FragmentSubjectListBinding;
 import android.mahendra.attendancemanager.databinding.ListItemSubjectBinding;
 import android.mahendra.attendancemanager.models.Subject;
 import android.mahendra.attendancemanager.viewmodels.SubjectListViewModel;
 import android.mahendra.attendancemanager.viewmodels.SubjectViewModel;
-import android.os.Binder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,7 +15,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
