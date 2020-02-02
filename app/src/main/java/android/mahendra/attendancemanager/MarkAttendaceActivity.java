3ab5834180907aff2e6ca7d501a60e9d902093b5
mahendra1290 <mahendra.suthar.1290@gmail.com>
2020-02-02 23:19:15 +0530

    viewpager working period fragment displaying

--- app/src/main/java/android/mahendra/attendancemanager/MarkAttendaceActivity.java
+++ app/src/main/java/android/mahendra/attendancemanager/MarkAttendaceActivity.java
@@ -8,9 +8,17 @@ import android.mahendra.attendancemanager.viewmodels.PeriodListViewModel;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkAttendaceActivity extends AppCompatActivity {
     private static final String TAG = "MarkAttendaceActivity";
@@ -20,6 +28,8 @@ public class MarkAttendaceActivity extends AppCompatActivity {
 
     private int mWeekDay;
     private PeriodListViewModel mViewModel;
+    private ViewPager2 mPeriodViewPager;
+    private PeriodPagerAdapter mPagerAdapter;
 
     public static Intent newIntent(Context context, int weekDay) {
         Intent i = new Intent(context, MarkAttendaceActivity.class);
@@ -32,7 +42,9 @@ public class MarkAttendaceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mWeekDay = getIntent().getIntExtra(EXTRA_WEEKDAY, 1);
         setContentView(R.layout.activity_mark_attendance);
-
+        mPeriodViewPager = findViewById(R.id.period_viewpager);
+        mPagerAdapter = new PeriodPagerAdapter(this);
+        mPeriodViewPager.setAdapter(mPagerAdapter);
         mViewModel = new ViewModelProvider(this).get(PeriodListViewModel.class);
         mViewModel.getAllSubjectsOn(mWeekDay).observe(this, subjects -> {
             for (Subject subject : subjects) {
@@ -44,6 +56,31 @@ public class MarkAttendaceActivity extends AppCompatActivity {
             for (Period period : periods) {
                 Log.i(TAG, "period " + period.getSubjectTitle() + "weekday " + period.getWeekDay());
             }
+            mPagerAdapter.setPeriods(periods);
         });
+    }
+
+    private class PeriodPagerAdapter extends FragmentStateAdapter {
+        List<Period> mPeriods = new ArrayList<>();
+
+        public PeriodPagerAdapter(FragmentActivity activity) {
+            super(activity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return PeriodFragment.newInstance(mPeriods.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPeriods.size();
+        }
+
+        public void setPeriods(List<Period> periods) {
+            mPeriods = periods;
+            notifyDataSetChanged();
+        }
     }
 }
