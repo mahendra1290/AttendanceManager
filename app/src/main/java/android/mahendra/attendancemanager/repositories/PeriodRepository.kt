fea5227415767ac639d03569279ebe21d8065058
mahendra1290 <mahendra.suthar.1290@gmail.com>
2020-03-25 12:58:58 +0530

    new methods added

--- app/src/main/java/android/mahendra/attendancemanager/repositories/PeriodRepository.kt
+++ app/src/main/java/android/mahendra/attendancemanager/repositories/PeriodRepository.kt
@@ -2,6 +2,7 @@ package android.mahendra.attendancemanager.repositories
 
 import android.app.Application
 import android.mahendra.attendancemanager.daos.PeriodDao
+import android.mahendra.attendancemanager.daos.SubjectDao
 import android.mahendra.attendancemanager.daos.SubjectPeriodDao
 import android.mahendra.attendancemanager.database.MainDatabase
 import android.mahendra.attendancemanager.models.Period
@@ -9,17 +10,11 @@ import android.mahendra.attendancemanager.models.Subject
 
 import androidx.lifecycle.LiveData
 
-class PeriodRepository(application: Application) {
-    private val periodDao: PeriodDao
-    private val subjectPeriodDao: SubjectPeriodDao
-    val allPeriods: LiveData<List<Period>>
-
-    init {
-        val db = MainDatabase.getDatabase(application)
-        periodDao = db.periodDao()
-        subjectPeriodDao = db.subjectPeriodDao()
-        allPeriods = periodDao.getAllPeriods()
-    }
+class PeriodRepository private constructor(
+        private val periodDao: PeriodDao,
+        private val subjectPeriodDao: SubjectPeriodDao
+) {
+    val allPeriods: LiveData<List<Period>> = periodDao.getAllPeriods()
 
     suspend fun insert(period: Period) {
             periodDao.insert(period)
@@ -51,5 +46,15 @@ class PeriodRepository(application: Application) {
 
     suspend fun deletePeriod(periodNumber: Int, weekDay: Int) {
         periodDao.deletePeriod(periodNumber, weekDay)
+    }
+
+    companion object {
+
+        @Volatile private var instance: PeriodRepository? = null
+
+        fun getInstance(periodDao: PeriodDao, subjectPeriodDao: SubjectPeriodDao) =
+                instance ?: synchronized(this) {
+                    instance ?: PeriodRepository(periodDao, subjectPeriodDao).also { instance = it }
+                }
     }
 }
