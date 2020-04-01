bb1c42e80b7b181db182e55832180307427a9885
mahendra1290 <mahendra.suthar.1290@gmail.com>
2020-04-01 13:24:18 +0530

    data binding implemented

--- app/src/main/java/android/mahendra/attendancemanager/dialogs/SubjectOptionBottomSheetDialog.kt
+++ app/src/main/java/android/mahendra/attendancemanager/dialogs/SubjectOptionBottomSheetDialog.kt
@@ -2,27 +2,29 @@ package android.mahendra.attendancemanager.dialogs
 
 import android.content.Context
 import android.mahendra.attendancemanager.R
+import android.mahendra.attendancemanager.databinding.BottomSheetSubjectBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class SubjectOptionBottomSheetDialog : BottomSheetDialogFragment(), View.OnClickListener {
-    private var subjectOptionListener: SubjectOptionListener? = null
+class SubjectOptionBottomSheetDialog : BottomSheetDialogFragment() {
+    private var clickListener: OptionClickListener? = null
+    private lateinit var subjectTitle: String
 
-    interface SubjectOptionListener {
-        fun onDeleteSelected(subjectTitle: String)
-        fun onEditTitleSelected(subjectTitle: String)
-        fun onEditAttendanceSelected(subjectTitle: String)
-        fun onResetAttendanceSelected(subjectTitle: String)
+    interface OptionClickListener {
+        fun onDelete(subjectTitle: String)
+        fun onEditTitle(subjectTitle: String)
+        fun onEditAttendance(subjectTitle: String)
+        fun onResetAttendance(subjectTitle: String)
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        subjectOptionListener = try {
-            targetFragment as SubjectOptionListener?
+        clickListener = try {
+            targetFragment as OptionClickListener
         } catch (ex: ClassCastException) {
             throw ClassCastException(context.toString() +
                     "must implement SubjectOptionBottomSheetDialog.SubjectOptionListener")
@@ -31,7 +33,7 @@ class SubjectOptionBottomSheetDialog : BottomSheetDialogFragment(), View.OnClick
 
     override fun onDetach() {
         super.onDetach()
-        subjectOptionListener = null
+        clickListener = null
     }
 
     override fun getTheme(): Int {
@@ -39,25 +41,27 @@ class SubjectOptionBottomSheetDialog : BottomSheetDialogFragment(), View.OnClick
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.bottom_sheet_subject, container, false)
-        val title = view.findViewById<TextView>(R.id.subject_title_modal_sheet)
-        title.text = requireArguments().getString(ARG_SUBJECT_TITLE, " ")
-        view.findViewById<View>(R.id.delete_subject_option).setOnClickListener(this)
-        view.findViewById<View>(R.id.edit_title_option).setOnClickListener(this)
-        view.findViewById<View>(R.id.edit_attendance_option).setOnClickListener(this)
-        view.findViewById<View>(R.id.reset_attendance_option).setOnClickListener(this)
-        return view
+        val binding = DataBindingUtil.inflate<BottomSheetSubjectBinding>(
+                inflater, R.layout.bottom_sheet_subject, container, false
+        )
+        subjectTitle = requireArguments().getString(ARG_SUBJECT_TITLE, " ")
+        binding.subjectTitle = subjectTitle
+
+        with (binding) {
+            deleteSubjectOption.setOptionClickListener(clickListener!!::onDelete)
+            editAttendanceOption.setOptionClickListener(clickListener!!::onEditAttendance)
+            resetAttendanceOption.setOptionClickListener(clickListener!!::onResetAttendance)
+            editTitleOption.setOptionClickListener(clickListener!!::onEditTitle)
+        }
+
+        return binding.root
     }
 
-    override fun onClick(v: View) {
-        val subjectTitle = requireArguments().getString(ARG_SUBJECT_TITLE, "")
-        when (v.id) {
-            R.id.delete_subject_option -> subjectOptionListener!!.onDeleteSelected(subjectTitle)
-            R.id.edit_title_option -> subjectOptionListener!!.onEditTitleSelected(subjectTitle)
-            R.id.edit_attendance_option -> subjectOptionListener!!.onEditAttendanceSelected(subjectTitle)
-            R.id.reset_attendance_option -> subjectOptionListener!!.onResetAttendanceSelected(subjectTitle)
+    private fun View.setOptionClickListener(onClick: (subjectTitle: String) -> Unit) {
+        setOnClickListener {
+            onClick(this@SubjectOptionBottomSheetDialog.subjectTitle)
+            this@SubjectOptionBottomSheetDialog.dismiss()
         }
-        dismiss()
     }
 
     companion object {
